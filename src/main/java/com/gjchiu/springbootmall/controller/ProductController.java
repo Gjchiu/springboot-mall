cb2351e9ea5afc7602b7725724633a2801cba5aa
gjchiu <george33669@gmail.com>
2024-03-21 17:00:42 +0800

    商品功能 - 實作刪除商品

--- src/main/java/com/gjchiu/springbootmall/controller/ProductController.java
+++ src/main/java/com/gjchiu/springbootmall/controller/ProductController.java
@@ -50,4 +50,12 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
 
     }
+
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable Integer productId){
+        productService.deleteProduct(productId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+
+    }
 }
--- src/main/java/com/gjchiu/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/gjchiu/springbootmall/dao/impl/ProductDaoImpl.java
@@ -81,4 +81,13 @@ public class ProductDaoImpl implements ProductDao {
 
         namedParameterJdbcTemplate.update(sql, map);
     }
+
+    @Override
+    public void deleteProduct(Integer productId) {
+        final String sql = "delete from product where product_id = :productId";
+        Map<String,Object> map = new HashMap<>();
+        map.put("productId", productId);
+
+        namedParameterJdbcTemplate.update(sql, map);
+    }
 }
