ea8522b8edbb3c07368aea6b7b289e522ee042ce
gjchiu <george33669@gmail.com>
2024-03-22 16:23:25 +0800

    商品功能 - 提煉程式

--- src/main/java/com/gjchiu/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/gjchiu/springbootmall/dao/impl/ProductDaoImpl.java
@@ -102,15 +102,7 @@ public class ProductDaoImpl implements ProductDao {
         Map<String,Object> map = new HashMap<>();
 
         //查詢條件
-        if(productQueryParams.getCategory() != null){
-            sql = sql + " and category = :category";
-            map.put("category",productQueryParams.getCategory().name());
-        }
-
-        if(productQueryParams.getSearch() != null){
-            sql = sql + " and product_name like :search";
-            map.put("search", "%" + productQueryParams.getSearch() + "%");
-        }
+        sql = addFilteringSql(sql, map, productQueryParams);
 
         //排序
         sql = sql + " order by " + productQueryParams.getOrder() + " " + productQueryParams.getSort();
@@ -132,6 +124,14 @@ public class ProductDaoImpl implements ProductDao {
         Map<String,Object> map = new HashMap<>();
 
         //查詢條件
+        sql = addFilteringSql(sql, map, productQueryParams);
+
+        Integer total = namedParameterJdbcTemplate.queryForObject(sql, map, Integer.class);
+
+        return total;
+    }
+
+    private String addFilteringSql(String sql, Map map, ProductQueryParams productQueryParams){
         if(productQueryParams.getCategory() != null){
             sql = sql + " and category = :category";
             map.put("category",productQueryParams.getCategory().name());
@@ -142,8 +142,6 @@ public class ProductDaoImpl implements ProductDao {
             map.put("search", "%" + productQueryParams.getSearch() + "%");
         }
 
-        Integer total = namedParameterJdbcTemplate.queryForObject(sql, map, Integer.class);
-
-        return total;
+        return sql;
     }
 }
